520f274e7333af0aaac7cba39a69474edf9a5935
youShouldHireDavid <lowerydd@colorado.edu>
2015-10-01 22:55:24 -0500

    Add updated routes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+# Bootstrap::Application.routes.draw.do
+
+  get '/about' 		=> 'high_voltage/pages#show', id: 'about'
+  get '/contact'	=> 'high_voltage/pages#show', id: 'contact'
+  get '/privacy'	=> 'high_voltage/pages#show', id: 'privacy'
+  get '/terms'		=> 'high_voltage/pages#show', id: 'terms'
+
+  get '/home', to: redirect('/')
+
+  root :to => 'high_voltage/pages#show', id: 'home'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
